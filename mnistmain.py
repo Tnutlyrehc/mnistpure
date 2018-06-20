82fd1484f6881030c992994ce72b577a019a7645
C <39905165+Tnutlyrehc@users.noreply.github.com>
2018-06-20 20:55:55 +0200

    +

--- mnistmain.py
+++ mnistmain.py
@@ -11,15 +11,16 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 def cnn_model_fn(features, labels, mode):
   # Input Layer
-  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # MNIST images are 28x28 pixels, and have one color channel
+  # Layer is defined and shaped -> going for a 4D tensor, 'cause a 3D isn't enough. Ever.
+        #4-D tensor: [batch_size, width, height, channels]
+  # The images are 28x28px, colorchannel 1
   input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-  # Convolutional Layer #1
-  # Computes 32 features using a 5x5 filter with ReLU activation.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 28, 28, 1]
-  # Output Tensor Shape: [batch_size, 28, 28, 32]
+  # Convolutional layer 1
+  # 5x5 filter w. ReLu activation.
+  # Input tensorshape = batchsize, 28x28 CC=1
+  # Output tensorshape = batchsize, 28x28 CC=21
+  #ReLu activation used
   conv1 = tf.layers.conv2d(
       inputs=input_layer,
       filters=32,
@@ -27,17 +28,16 @@ def cnn_model_fn(features, labels, mode):
       padding="same",
       activation=tf.nn.relu)
 
-  # Pooling Layer #1
-  # First max pooling layer with a 2x2 filter and stride of 2
+  # Poooling layer no. 1
+  # max poollayer 2x2 - input = batch, 28x28, 32 + stride of 2
   # Input Tensor Shape: [batch_size, 28, 28, 32]
   # Output Tensor Shape: [batch_size, 14, 14, 32]
   pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
   # Convolutional Layer #2
-  # Computes 64 features using a 5x5 filter.
-  # Padding is added to preserve width and height.
   # Input Tensor Shape: [batch_size, 14, 14, 32]
   # Output Tensor Shape: [batch_size, 14, 14, 64]
+  # computing features x 64 - using 5x5 --> padded to preserve width * height
   conv2 = tf.layers.conv2d(
       inputs=pool1,
       filters=64,
@@ -46,7 +46,6 @@ def cnn_model_fn(features, labels, mode):
       activation=tf.nn.relu)
 
   # Pooling Layer #2
-  # Second max pooling layer with a 2x2 filter and stride of 2
   # Input Tensor Shape: [batch_size, 14, 14, 64]
   # Output Tensor Shape: [batch_size, 7, 7, 64]
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
@@ -67,24 +66,21 @@ def cnn_model_fn(features, labels, mode):
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
   # Logits layer
-  # Input Tensor Shape: [batch_size, 1024]
-  # Output Tensor Shape: [batch_size, 10]
+  # input tensor - batchsize 1024, output tensor - batchsize 10
   logits = tf.layers.dense(inputs=dropout, units=10)
 
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
       "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-  # Calculate Loss (for both TRAIN and EVAL modes)
+  # Loss calculation - train + eval mode
   loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
-  # Configure the Training Op (for TRAIN mode)
+  # Training - TRAIN mode
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
     train_op = optimizer.minimize(
